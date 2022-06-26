2cb6d7952396623036d66f0dc33c13032b19e15c
pabloskewes <pablo.skewes@ug.uchile.cl>
2022-06-27 01:35:00 +0200

    new features added
    - text color changed to black
    - blank text not allowed for new todolists and items
    - automatic keyboard hide after adding new todolists or items

--- app/src/main/java/com/example/newtp2/ChoixListActivity.kt
+++ app/src/main/java/com/example/newtp2/ChoixListActivity.kt
@@ -1,7 +1,11 @@
 package com.example.newtp2
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_choix_list.*
 
@@ -19,8 +23,18 @@ class ChoixListActivity : AppCompatActivity() {
 
         btnAddTodo.setOnClickListener {
             val new_todo = etNewList.text.toString()
-            todolists.add(new_todo)
-            adapter.notifyItemInserted(todolists.size - 1)
+            if (new_todo.isBlank()) {
+                Toast.makeText(this, "Text cannot be blank", Toast.LENGTH_SHORT).show()
+            } else {
+                todolists.add(new_todo)
+                adapter.notifyItemInserted(todolists.size - 1)
+                hideSoftKeyboard(it)
+            }
         }
+    }
+
+    private fun hideSoftKeyboard(view: View) {
+        val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        manager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
--- app/src/main/java/com/example/newtp2/ShowListActivity.kt
+++ app/src/main/java/com/example/newtp2/ShowListActivity.kt
@@ -1,7 +1,11 @@
 package com.example.newtp2
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_show_list.*
 
@@ -21,10 +25,20 @@ class ShowListActivity : AppCompatActivity() {
 
         btnAddItem.setOnClickListener{
             val title = etNewItem.text.toString()
-            val todo = Todo(title, false)
-            todoList.add(todo)
-            adapter.notifyItemInserted(todoList.size - 1)
+            if (title.isBlank()) {
+                Toast.makeText(this, "Text cannot be blank", Toast.LENGTH_SHORT).show()
+            } else {
+                val todo = Todo(title, false)
+                todoList.add(todo)
+                adapter.notifyItemInserted(todoList.size - 1)
+                hideSoftKeyboard(it)
+            }
         }
+    }
 
+    private fun hideSoftKeyboard(view: View) {
+        val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        manager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
+
